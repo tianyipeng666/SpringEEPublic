2e744f37cfd71b512bb63ff00448c7f586dd9f9d
tianyipeng666 <506575523@qq.com>
2024-06-22 14:39:58 +0800

    firstCommit

---
+++ SpringBoot/src/main/java/com/typ/MainStart.java
@@ -0,0 +1,11 @@
+package com.typ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainStart {
+    public static void main(String[] args) {
+        SpringApplication.run(MainStart.class, args);
+    }
+}
